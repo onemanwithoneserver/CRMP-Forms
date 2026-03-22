ed8b453cb155f04688b0b24495a43bbb05de1d02
Mamidi Lakshman Mohan Manikanta <onemanwithoneserver@gmail.com>
2026-03-22 14:18:40 +0530

    Git Pages

--- forms/src/main.tsx
+++ forms/src/main.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/CRMP-Forms">
         <App />
       </BrowserRouter>
     </QueryClientProvider>

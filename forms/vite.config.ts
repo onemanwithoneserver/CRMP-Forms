46bf6d79dc8bef7251d6c62e639b091708bdfffb
Mamidi Lakshman Mohan Manikanta <onemanwithoneserver@gmail.com>
2026-03-29 09:03:11 +0530

    GitPages

--- forms/vite.config.ts
+++ forms/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CRMP-Forms/', // Ensure this matches your repo name
+  base: '/CREMP-Forms/', 
   plugins: [react()],
   server: {
     port: 3000
